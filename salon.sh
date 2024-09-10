2e2963ada7810ebd010e0b7b60e672b05a946d2a
D0M <84682403+thedcollins@users.noreply.github.com>
2024-09-10 15:39:40 +0100

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -68,49 +68,289 @@ HAIR_SERVICES() {
 # Function to handle nail services
 NAIL_SERVICES() {
   echo -e "\nYou have selected Nail Services."
-  # Add specific actions for nail services here
+  echo -e "\nWhat's your phone number?"
+  read CUSTOMER_PHONE
+
+  CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+
+  # If customer doesn't exist
+  if [[ -z $CUSTOMER_NAME ]]
+  then
+    # Get new customer name
+    echo -e "\nI don't have a record for that phone number, what's your name?"
+    read CUSTOMER_NAME
+
+    # Insert new customer
+    INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
+  fi
+
+  # Get customer_id
+  CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+
+  # Ask for service time
+  echo -e "\nWhat time would you like your nail appointment?"
+  read SERVICE_TIME
+
+  # Insert appointment
+  INSERT_CUSTOMER_INFO=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+
+  # Get service
+  SERVICE=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+
+  # Confirm appointment
+  echo -e "\nI have put you down for a $SERVICE at $SERVICE_TIME, $CUSTOMER_NAME."
 }
 
 # Function to handle skin and facial services
 SKIN_AND_FACIAL_SERVICES() {
   echo -e "\nYou have selected Skin and Facial Services."
-  # Add specific actions for skin and facial services here
+  echo -e "\nWhat's your phone number?"
+  read CUSTOMER_PHONE
+
+  CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+
+  # If customer doesn't exist
+  if [[ -z $CUSTOMER_NAME ]]
+  then
+    # Get new customer name
+    echo -e "\nI don't have a record for that phone number, what's your name?"
+    read CUSTOMER_NAME
+
+    # Insert new customer
+    INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
+  fi
+
+  # Get customer_id
+  CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+
+  # Ask for service time
+  echo -e "\nWhat time would you like your skin and facial appointment?"
+  read SERVICE_TIME
+
+  # Insert appointment
+  INSERT_CUSTOMER_INFO=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+
+  # Get service
+  SERVICE=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+
+  # Confirm appointment
+  echo -e "\nI have put you down for a $SERVICE at $SERVICE_TIME, $CUSTOMER_NAME."
 }
 
 # Function to handle massage services
 MASSAGE_SERVICES() {
   echo -e "\nYou have selected Massage Services."
-  # Add specific actions for massage services here
+  echo -e "\nWhat's your phone number?"
+  read CUSTOMER_PHONE
+
+  CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+
+  # If customer doesn't exist
+  if [[ -z $CUSTOMER_NAME ]]
+  then
+    # Get new customer name
+    echo -e "\nI don't have a record for that phone number, what's your name?"
+    read CUSTOMER_NAME
+
+    # Insert new customer
+    INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
+  fi
+
+  # Get customer_id
+  CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+
+  # Ask for service time
+  echo -e "\nWhat time would you like your massage appointment?"
+  read SERVICE_TIME
+
+  # Insert appointment
+  INSERT_CUSTOMER_INFO=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+
+  # Get service
+  SERVICE=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+
+  # Confirm appointment
+  echo -e "\nI have put you down for a $SERVICE at $SERVICE_TIME, $CUSTOMER_NAME."
 }
 
 # Function to handle makeup services
 MAKEUP_SERVICES() {
   echo -e "\nYou have selected Makeup Services."
-  # Add specific actions for makeup services here
+  echo -e "\nWhat's your phone number?"
+  read CUSTOMER_PHONE
+
+  CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+
+  # If customer doesn't exist
+  if [[ -z $CUSTOMER_NAME ]]
+  then
+    # Get new customer name
+    echo -e "\nI don't have a record for that phone number, what's your name?"
+    read CUSTOMER_NAME
+
+    # Insert new customer
+    INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
+  fi
+
+  # Get customer_id
+  CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+
+  # Ask for service time
+  echo -e "\nWhat time would you like your makeup appointment?"
+  read SERVICE_TIME
+
+  # Insert appointment
+  INSERT_CUSTOMER_INFO=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+
+  # Get service
+  SERVICE=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+
+  # Confirm appointment
+  echo -e "\nI have put you down for a $SERVICE at $SERVICE_TIME, $CUSTOMER_NAME."
 }
 
 # Function to handle body treatments
 BODY_TREATMENTS() {
   echo -e "\nYou have selected Body Treatments."
-  # Add specific actions for body treatments here
+  echo -e "\nWhat's your phone number?"
+  read CUSTOMER_PHONE
+
+  CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+
+  # If customer doesn't exist
+  if [[ -z $CUSTOMER_NAME ]]
+  then
+    # Get new customer name
+    echo -e "\nI don't have a record for that phone number, what's your name?"
+    read CUSTOMER_NAME
+
+    # Insert new customer
+    INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
+  fi
+
+  # Get customer_id
+  CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+
+  # Ask for service time
+  echo -e "\nWhat time would you like your body treatment appointment?"
+  read SERVICE_TIME
+
+  # Insert appointment
+  INSERT_CUSTOMER_INFO=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+
+  # Get service
+  SERVICE=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+
+  # Confirm appointment
+  echo -e "\nI have put you down for a $SERVICE at $SERVICE_TIME, $CUSTOMER_NAME."
 }
 
 # Function to handle men’s grooming services
 MENS_GROOMING_SERVICES() {
   echo -e "\nYou have selected Men’s Grooming Services."
-  # Add specific actions for men’s grooming services here
+  echo -e "\nWhat's your phone number?"
+  read CUSTOMER_PHONE
+
+  CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+
+  # If customer doesn't exist
+  if [[ -z $CUSTOMER_NAME ]]
+  then
+    # Get new customer name
+    echo -e "\nI don't have a record for that phone number, what's your name?"
+    read CUSTOMER_NAME
+
+    # Insert new customer
+    INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
+  fi
+
+  # Get customer_id
+  CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+
+  # Ask for service time
+  echo -e "\nWhat time would you like your men’s grooming appointment?"
+  read SERVICE_TIME
+
+  # Insert appointment
+  INSERT_CUSTOMER_INFO=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+
+  # Get service
+  SERVICE=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+
+  # Confirm appointment
+  echo -e "\nI have put you down for a $SERVICE at $SERVICE_TIME, $CUSTOMER_NAME."
 }
 
 # Function to handle spa packages
 SPA_PACKAGES() {
   echo -e "\nYou have selected Spa Packages."
-  # Add specific actions for spa packages here
+  echo -e "\nWhat's your phone number?"
+  read CUSTOMER_PHONE
+
+  CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+
+  # If customer doesn't exist
+  if [[ -z $CUSTOMER_NAME ]]
+  then
+    # Get new customer name
+    echo -e "\nI don't have a record for that phone number, what's your name?"
+    read CUSTOMER_NAME
+
+    # Insert new customer
+    INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
+  fi
+
+  # Get customer_id
+  CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+
+  # Ask for service time
+  echo -e "\nWhat time would you like your spa package appointment?"
+  read SERVICE_TIME
+
+  # Insert appointment
+  INSERT_CUSTOMER_INFO=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+
+  # Get service
+  SERVICE=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+
+  # Confirm appointment
+  echo -e "\nI have put you down for a $SERVICE at $SERVICE_TIME, $CUSTOMER_NAME."
 }
 
 # Function to handle other specialty services
 OTHER_SPECIALTY_SERVICES() {
   echo -e "\nYou have selected Other Specialty Services."
-  # Add specific actions for other specialty services here
+  echo -e "\nWhat's your phone number?"
+  read CUSTOMER_PHONE
+
+  CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+
+  # If customer doesn't exist
+  if [[ -z $CUSTOMER_NAME ]]
+  then
+    # Get new customer name
+    echo -e "\nI don't have a record for that phone number, what's your name?"
+    read CUSTOMER_NAME
+
+    # Insert new customer
+    INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
+  fi
+
+  # Get customer_id
+  CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+
+  # Ask for service time
+  echo -e "\nWhat time would you like your specialty service appointment?"
+  read SERVICE_TIME
+
+  # Insert appointment
+  INSERT_CUSTOMER_INFO=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+
+  # Get service
+  SERVICE=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+
+  # Confirm appointment
+  echo -e "\nI have put you down for a $SERVICE at $SERVICE_TIME, $CUSTOMER_NAME."
 }
 
 # Function to handle exiting the menu
